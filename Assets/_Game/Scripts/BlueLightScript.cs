a8db45b40785434ee9a476ba0f7afd32b841e0aa
C64Gamer <info@c64gamer.com>
2019-04-28 00:41:15 +0200

    Auto stash before merge of "master" and "origin/master"
    Adding BlueLight

---
+++ Assets/_Game/Scripts/BlueLightScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LdJam44
+{
+    public class BlueLightScript : MonoBehaviour
+    {
+
+        [Header("General")]
+        public float LightSpeed;
+        public bool isBlueLightOn;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isBlueLightOn) {
+                transform.Rotate(0, LightSpeed, 0);
+            }
+        }
+    }
+}
