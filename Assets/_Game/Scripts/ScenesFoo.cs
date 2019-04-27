3a4cd6e5a5c3f044e01b2a90519ed4f4bff4cd98
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-04-27 23:03:12 +0200

    wip

---
+++ Assets/_Game/Scripts/ScenesFoo.cs
@@ -0,0 +1,8 @@
+namespace LdJam44
+{
+    public static class Scenes
+    {
+        public static readonly string Game = "Game";
+        public static readonly string MrUnicornator = "MrUnicornator";
+    }
+}
---
+++ Assets/_Game/Scripts/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace LdJam44.UI
+{
+    public class MainMenu : UIBehaviour
+    {
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        public void StartGame()
+        {
+#if DEBUG
+            SceneManager.LoadScene(Scenes.MrUnicornator);
+#else
+            SceneManager.LoadScene(Scenes.Game);
+#endif
+        }
+    }
+}
