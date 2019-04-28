fd67965d1014b20e74abeb04355c55d7a06fa780
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-04-28 02:18:49 +0200

    Light Settings & Lalülala

--- Assets/_Game/Scripts/BlueLightScript.cs
+++ Assets/_Game/Scripts/BlueLightScript.cs
@@ -1,28 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace LdJam44
 {
     public class BlueLightScript : MonoBehaviour
     {
 
-        [Header("General")]
-        public float LightSpeed;
-        public bool isBlueLightOn;
-        
-        // Start is called before the first frame update
-        void Start()
-        {
+        [Header("Variables")]
+        public Light[] Lights;
 
+        public float FlashTime = 0.25f;
+        public bool IsBlueLightOn;
+
+        private int _nextLight;
+        private float _timeToNextLightChange;
+
+        private void Start()
+        {
+            _timeToNextLightChange = Time.time;
         }
 
-        // Update is called once per frame
         void Update()
         {
-            if (isBlueLightOn) {
-                transform.Rotate(0, LightSpeed, 0);
+            if (!IsBlueLightOn || Time.time < _timeToNextLightChange)
+            {
+                return;
             }
+
+            _timeToNextLightChange = Time.time + FlashTime;
+            
+            foreach (var light in Lights)
+            {
+                light.gameObject.SetActive(false);
+            }
+            
+            Lights[_nextLight].gameObject.SetActive(true);
+
+            _nextLight = ++_nextLight % Lights.Length;
         }
     }
 }
